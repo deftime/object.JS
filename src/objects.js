02f35064ad27fb6b1daae57c352a5763947d3671
Volodymyr Gorbunov <deftime@gmail.com>
2022-02-18 09:59:59 +0200

    Random number math function.

--- src/objects.js
+++ src/objects.js
@@ -284,3 +284,12 @@ let action = {
         })
     }
 }
+
+// Mathematics and other operations with digits
+let mathCount = {
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
